8875e34990c31f3b4b953b74d3b845bb9779c452
TylerGubala <gubalatyler@gmail.com>
2017-07-29 21:42:22 -0400

    Finalized Private class

--- src/privatizors.js
+++ src/privatizors.js
@@ -3,27 +3,73 @@
         (typeof Proxy.prototype === 'undefined'? Proxy.prototype={}: null);
         class Private extends Proxy{
             constructor(object, handler){
-                Object.assign(handler, {
-
-                })
-                super(object, {
-                    apply: ('apply' in handler? handler.apply : undefined),
-                    construct: ('construct' in handler? handler.construct : undefined),
-                    defineProperty: function(target, property){
-
-                    },
-                    deleteProperty: function(target, property){
-
-                    },
+                super(object, Object.assign( {}, handler, {
                     get: function(target, property, reciever){
-                        if(property.startsWith('_')){
-                            return
+                        if(property.startsWith("_")){
+                            return undefined;
                         }
                         else{
-                            return ('get' in handler? handler.get(target, property, handler) : target[property]);
+                            return ("get" in handler? handler.get(target, property, reciever) : target["property"])
+                        }
+                    },
+                    set: function(target, property, value, reciever){
+                        if(property.startsWith("_")){
+                            return false;
+                        }
+                        else{
+                            if("set" in handler){
+                                return handler.set(target, property, value, reciever)
+                            }
+                            else{
+                                target[property] = value;
+                                return true;
+                            }
+                        }
+                    },
+                    has: function(target, property){
+                        if (property.startsWith("_")){
+                            return false;
+                        }
+                        else{
+                            if ("has" in handler){
+                                return handler.has(target, property);
+                            }
+                            else{
+                                return property in handler;
+                            }
+                        }
+                    },
+                    defineProperty: function(target, property, descriptor){
+                        if(property.startsWith("_")){
+                            return false;
+                        }
+                        else{
+                            return ("defineProperty" in handler? handler.defineProperty(target, property, descriptor): Object.defineProperty(target, property, descriptor));
+                        }
+                    },
+                    deleteProperty: function(target, property){
+                        if (property.startsWith("_")){
+                            return false;
+                        }
+                        else{
+                            if ("deleteProperty" in handler){
+                                return handler.deleteProperty(target, property);
+                            }
+                            else{
+                                delete target[property];
+                                return true;
+                            }
+                        }
+                    },
+                    ownKeys: function(target){
+                        if ("ownKeys" in handler){
+                            return handler.ownKeys(target);
+                        }
+                        else{
+                            return Object.keys(target).filter(function(key){return (!key.startsWith("_"))});
                         }
                     }
-                })
+                }))
             }
         }
         exports.Private = Private;
